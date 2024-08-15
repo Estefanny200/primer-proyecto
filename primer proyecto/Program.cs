dabd4de02c38e05670a4130be559dc534d6cf4b0
Estefanny200 <estefannybarahona37@gmail.com>
2024-08-15 13:35:03 -0600

    actualizacion de program de primer proyecto
    recibir y convertir datos

--- primer proyecto/Program.cs
+++ primer proyecto/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
+
+
+
+        Console.WriteLine("Ingresa tu nombre para continuar");
+        string Nombre = Consola.ReadLine();
+        Console.WriteLine("Ingrese si edad");
+        int Edad = Convert.ToInt32(Console.ReadLine());
+        double EdadMeses = Edad * 12;
+        Console.WriteLine(Nombre + "Tu edad em Meses es: " + EdadMeses + "Meses");
+    }
+
